1cf09fc5844c1d013d0786fac3c4349968d2dae8
Robin Lee Powell <robinleepowell@gmail.com>
2020-12-28 23:07:10 -0800

    Updated for influxdb and general tweaks

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# run with "sudo bash -x" or similar
+
+setenforce 0
+
 virsh shutdown hassio
 
 sleep 3
@@ -8,6 +12,10 @@ sleep 3
 
 virsh undefine hassio
 
-virt-builder ubuntu-18.04 -v --output /var/lib/libvirt/images/hassio.raw --format raw --size 20G --root-password password:test123 --hostname hassio --run /srv/hassio/hassio.run.sh
+virt-builder ubuntu-18.04 -v --output /var/lib/libvirt/images/hassio.raw --format raw --size 20G --root-password password:test123 --hostname hassio --firstboot /srv/hassio/hassio.run.sh
 
-virt-install --name hassio --import --ram 4096 --os-variant ubuntu18.04 --autostart --disk /var/lib/libvirt/images/hassio.raw --graphics none --filesystem /srv/hassio/data,hassio --console pty,target.type=virtio --network bridge=br0
+virt-install --name hassio --import --ram 4096 --os-variant ubuntu18.04 --autostart --disk /var/lib/libvirt/images/hassio.raw --graphics none --filesystem /srv/hassio/data-hassio,hassio --filesystem /srv/hassio/data-influxdb,influxdb --console pty,target.type=virtio --network bridge=br0 --noautoconsole
+
+setenforce 1
+
+echo -e "\n\n\nMain script output is in ~root/virt-sysprep-firstboot.log on the host.\n\nLog in as rlpowell / test123\n\n\nOnce you've logged in and checked that file, you have to REBOOT (to get the file system mounts working)\n\n\nAfter you've rebooted and check the file system mounts, run this by hand (because it talks to the tty):\n\ncurl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | sudo bash -s && sudo ha su repair && sudo ha host reboot\n\n\n"
--- hassio.run.sh
+++ hassio.run.sh
@@ -38,11 +38,11 @@ netplan apply
 echo 'rlpowell    ALL=(ALL)    TYPE=unconfined_t ROLE=unconfined_r   ALL' >/etc/sudoers.d/rlpowell
 apt-get install -y zsh vim
 
+# Please don't ask stupid questions
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+
 # Make sure ssh is awake
 dpkg-reconfigure openssh-server
-
-# Set up the mount point
-mkidr -p /usr/share/hassio
 
 # Install docker
 apt-get update
@@ -50,21 +50,26 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 apt-get update
-apt-get install -y docker-ce
+# The versions are because of https://alerts.home-assistant.io/#docker_2010.markdown
+apt-get install -y docker-ce=5:19.03.14~3-0~ubuntu-bionic docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic containerd.io=1.3.9-1
 systemctl enable docker
 
-# Set up hassio mount point
 echo 'loop
 virtio
 9p
 9pnet
 9pnet_virtio' >> /etc/modules
 
+# Set up the hassio mount point
 mkdir -p /usr/share/hassio
 echo 'hassio /usr/share/hassio 9p trans=virtio,version=9p2000.L 0 0' >>/etc/fstab
 
+# Set up the influxdb mount point
+apt-get install -y influxdb influxdb-client
+echo 'influxdb /var/lib/influxdb 9p trans=virtio,version=9p2000.L 0 0' >>/etc/fstab
+
 # Pepare to install hassio
-apt-get install -y jq avahi-daemon dbus
+apt-get install -y jq avahi-daemon dbus network-manager
 
 # Get the hassio cli
 wget https://github.com/home-assistant/cli/releases/latest/download/ha_amd64 -O /usr/local/bin/ha_amd64
@@ -76,5 +81,4 @@ cat >/usr/local/bin/ha <<'EOF'
 EOF
 chmod 755 /usr/local/bin/ha
 
-# Can't actually run this because it needs docker fully up and running
-echo 'After boot run: curl -sL https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh | sudo bash -s -x ; ha su repair ; ha host reboot'
+curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | bash -s && ha su repair && ha host reboot
